ccfe5b3b73c8d099eece52227a6e9df36c110713
LinYuXuan-judy <jujuove988@gmail.com>
2022-06-05 15:21:21 +0800

    add launcher hammer and bullet prefab

---
+++ assets/script/Hammer.ts
@@ -0,0 +1,22 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class Hammer extends cc.Component {
+
+    onLoad() {
+        cc.director.getPhysicsManager().enabled = true;
+    }
+
+    start() {
+        var sequence1 = cc.sequence(cc.moveBy(1, 0, 125), cc.moveBy(0.5, 0, -125));
+        var action = cc.repeatForever(sequence1);
+        this.node.runAction(action);
+    }
+}
---
+++ assets/script/Launcher.ts
@@ -0,0 +1,82 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class Launcher extends cc.Component {
+
+    @property(cc.Prefab)
+    private bulletPrefab: cc.Prefab = null;
+
+    private canCreateBullet: boolean = true;
+
+    private bulletInterval: number = 1.0;
+
+    private moveX: number = -480;
+
+    private moveY: number = -320;
+
+    private edgeX: number = 320; // 左到右的扇形面積
+    private edgeY: number = 320; // 上到下的扇形面積
+
+    onLoad() {
+        this.moveX = -this.edgeX - this.node.position.x;
+        this.moveY = -this.edgeY - this.node.position.y;
+    }
+
+
+    start() {
+    }
+
+
+    private findPlayer(dt) {
+        // a bullet can be created only when canCreateBullet is True
+        if (this.canCreateBullet) {
+            cc.log("a");
+            this.createBullet();
+        }
+    }
+
+
+    private createBullet() {
+        this.canCreateBullet = false;
+        //let bullet = null;
+        this.scheduleOnce(function () {
+            this.canCreateBullet = true;
+        }, this.bulletInterval);
+
+        // down
+        let bullet = cc.instantiate(this.bulletPrefab);
+        bullet.getComponent('Bullet').init(this.node, this.moveX, -320 - this.node.position.y); // -480, -320
+
+        // up
+        let bulletUp = cc.instantiate(this.bulletPrefab);
+        bulletUp.getComponent('Bullet').init(this.node, -this.moveX, 320 - this.node.position.y); // 480, 32
+
+        // left
+        let bulletLeft = cc.instantiate(this.bulletPrefab);
+        bulletLeft.getComponent('Bullet').init(this.node, this.edgeX - this.node.position.x, this.moveY); // 480, -320
+
+        // right
+        let bulletRight = cc.instantiate(this.bulletPrefab);
+        bulletRight.getComponent('Bullet').init(this.node, -this.edgeX - this.node.position.x, -this.moveY); // -480, 320
+
+        this.moveX += 80; // 移動多少
+        if (this.moveX == this.edgeX)
+            this.moveX = -this.edgeX - this.node.position.x;
+
+        this.moveY += 80; // 移動多少
+        if (this.moveY == this.edgeY)
+            this.moveY = -this.edgeY - this.node.position.y;
+
+    }
+
+    update(dt) {
+        this.findPlayer(dt);
+    }
+}
